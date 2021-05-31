29f2b524ccaf970175dffe934ff6b204173b4fb7
Sergiu Canschi <info@webdesign7.co.uk>
2021-05-31 12:15:50 +0100

    cherry pick

--- index2.php
+++ index2.php
@@ -5,5 +5,5 @@ echo "this is the second commit to master";
 echo "this is the line from feature/test1";
 
 echo  "commit 2";
-
-echo "this was supposed to go in test3";
+echo  "commit 3";
+echo  'commit 3 a';
